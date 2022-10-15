076087cdb62957c90d5ea14a49c49ec3ecba89df
CuddlyBunion341 <daniel@bengl.cz>
2022-10-15 18:50:36 +0200

    initial commit

---
+++ src/server/server.ts
@@ -0,0 +1,25 @@
+import express from 'express'
+import path from 'path'
+import http from 'http'
+
+const port: number = 3000
+
+class App {
+    private server: http.Server
+    private port: number
+
+    constructor(port: number) {
+        this.port = port
+        const app = express()
+        app.use(express.static(path.join(__dirname, '../client')))
+        this.server = new http.Server(app)
+    }
+
+    public Start() {
+        this.server.listen(this.port, () => {
+            console.log(`Server listening on port ${this.port}.`)
+        })
+    }
+}
+
+new App(port).Start()
