cba79cca5a1e42aff1669f588fdcadd7cbefc5ac
CuddlyBunion341 <daniel@bengl.cz>
2022-10-21 10:34:55 +0200

    added fractal noise

--- src/client/world/generator.ts
+++ src/client/world/generator.ts
@@ -3,17 +3,48 @@ import { createNoise2D, NoiseFunction2D } from 'simplex-noise'
 import { blockIDs } from '../blocks/blocks'
 import { Chunk } from './chunk'
 
+function fractalNoise2D(
+    noise: NoiseFunction2D,
+    x: number,
+    y: number,
+    octaves: number,
+    lacunarity: number,
+    persistence: number
+) {
+    let value = 0
+    let p = 1
+    let l = 1
+    for (let i = 0; i < octaves; i++) {
+        value += noise(x * l, y * l) * p
+        l *= lacunarity
+        p *= persistence
+    }
+    return value
+}
+
+function createFractalNoise2D(
+    noise: NoiseFunction2D,
+    octaves = 3,
+    lacunarity = 2,
+    persistence = 0.5
+): NoiseFunction2D {
+    return function (x: number, y: number) {
+        return fractalNoise2D(noise, x, y, octaves, lacunarity, persistence)
+    }
+}
+
 class TerrainGenerator {
     seed: number
     noise: NoiseFunction2D
-    hilliness = 10
+    hilliness = 20
     spread = 64
-    minHeight = 60
+    minHeight = 50
     seaLevel = 65
 
     constructor(seed: number = 69420) {
         this.seed = seed
         this.noise = createNoise2D(alea(seed))
+        this.noise = createFractalNoise2D(this.noise)
     }
 
     createHeightMap(originX: number, originZ: number, width: number, height: number) {
